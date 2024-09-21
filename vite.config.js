cad66c5b46904c0adf8bdcc5bc4d052a8f22e7d1
Oluwaseyi Toluhi <seyi@Oluwaseyis-Laptop.local>
2024-09-21 21:32:25 +0100

    vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    root: path.resolve(__dirname),
+    base: '/', 
+    test: {
+      globals: true,
+      environment: "jsdom",
+      coverage: {
+        enabled: true,
+        reporter: ['text', 'html'],
+      },
+    },
+    build: {
+      outDir: path.resolve(__dirname, 'dist'),
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+        },
+      },
+      emptyOutDir: true,
+    },
+  };
+});
