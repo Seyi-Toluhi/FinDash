35d1d3197885fbb57e425817c5f844dcf2cf5aa3
Oluwaseyi Toluhi <seyi@Oluwaseyis-Laptop.local>
2024-09-23 08:42:49 +0100

    components setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import Dashboard from './Dashboard';
+
 
 
 function App() {
   return (
     <>
-      <h1>Welcome to FinDash</h1>
-      <div id="lottie-animation" className="animation_container">
-        <dotlottie-player 
-          src="https://lottie.host/f60ed0b5-5927-41bf-bd92-160d9af3ee0e/difhaTwSYM.json" 
-          background="transparent" 
-          speed="1" 
-          style={{ width: "300px", height: "300px" }} 
-          loop 
-          autoplay>
-        </dotlottie-player>
-      </div>
+        < Dashboard/>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import App from './App.jsx'
-import './index.css'
+
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#8993FF", 
+    },
+    secondary: {
+      main: "#FEFEFE", 
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
     <App />
+    </ThemeProvider>
   </StrictMode>,
 )
